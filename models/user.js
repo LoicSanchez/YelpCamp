a9511c14fd8ed0dadbb8305e91d4a9084dc817c0
Loic <loic.sanchez@live.com>
2019-05-29 22:28:22 +0000

    update User model

--- models/user.js
+++ models/user.js
@@ -2,9 +2,14 @@ var mongoose                =   require("mongoose"),
     passportLocalMongoose   =   require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
-    username: String,
+    username: {type: String, unique: true, required: true},
     password: String,
-    isAdmin: {type: Boolean, default: false}
+    isAdmin: {type: Boolean, default: false},
+    email: String,
+    firstName: String,
+    lastName: String,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 });
 userSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User", userSchema);
